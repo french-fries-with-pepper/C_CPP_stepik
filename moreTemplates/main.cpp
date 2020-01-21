cdefd9949587547d7039a8d031f1250d025a4798
french-fries-with-pepper <alexey.s.antonov@zoho.com>
2020-01-21 18:18:56 +0300

    add more template classes

---
+++ moreTemplates/main.cpp
@@ -0,0 +1,33 @@
+struct ICloneable
+{
+    virtual ICloneable *clone() const = 0;
+    virtual ~ICloneable() {}
+};
+
+// Шаблон ValueHolder с типовым параметром T,
+// должен содержать одно открытое поле data_
+// типа T.
+//
+// В шаблоне ValueHolder должен быть определен
+// конструктор от одного параметра типа T,
+// который инициализирует поле data_.
+//
+// Шаблон ValueHolder должен реализовывать
+// интерфейс ICloneable, и возвращать указатель
+// на копию объекта, созданную в куче, из метода
+// clone.
+
+template <typename T>
+struct ValueHolder : ICloneable
+{
+    ValueHolder(const T &obj) : data_(obj)
+    {
+    }
+    ICloneable *clone() const
+    {
+
+        return new ValueHolder(*this);
+    }
+
+    T data_;
+}; // дальше самостоятельно
